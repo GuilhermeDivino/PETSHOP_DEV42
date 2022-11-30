ad0209981b1093c31d06b0621ac1b0374bac9798
Clayton Antonio Yokobataki <104380657+Yokobataki@users.noreply.github.com>
2022-11-30 12:35:26 -0300

    adicionado pagina gatos/passaros/peixes.html

---
+++ VENDAS/peixes.js
@@ -0,0 +1,74 @@
+let produtoJson = [
+    {
+        id: 1,
+        name: 'Ração Nutribetta Nutricon para Peixes 12g',
+        img: 'images/racao_peixe01.jpg',
+        price: [9.09, 9.09, 9.09],
+        sizes: [
+          '12g',
+          '12g',
+          '12g'
+        ],
+        description: 'Indicado para peixes;'
+    },
+    {
+        id: 2,
+        name: 'Alimento para Peixe Tetra Peixe TetraMin Flakes',
+        img: 'images/racao_peixe02.jpg',
+        price: [11.99, 11.99, 11.99],
+        sizes: [
+          '20g',
+          '20g',
+          '20g'
+        ],
+        description: ' Indicado para peixes tropicais;'
+    },
+    {
+        id: 3,
+        name: 'Ração Nutricon Bottom para Peixes',
+        img: 'images/racao_peixe03.jpg',
+        price: [12.99, 23.99, 23.99],
+        sizes: [
+          '50g',
+          '110g',
+          '110g'
+        ],
+        description: 'Indicado para peixes de estimação e invertebrados que habitam os fundos de aquários;'
+    },
+    {
+        id: 4,
+        name: 'Ração Nutricon Carnivoros Superfície para Peixes',
+        img: 'images/racao_passaro04.jpg',
+        price: [15.99, 40.99, 44.99],
+        sizes: [
+          '70g',
+          '225g',
+          '275g'
+        ],
+        description: 'Indicado para peixes carnívoros de estimação, de médio e grande porte em sua fase inicial;'
+    },
+    {
+        id: 5,
+        name: 'Alimento para peixe Alcon Guppy 20gr',
+        img: 'images/racao_passaro05.jpg',
+        price: [19.99, 19.99, 19.99],
+        sizes: [
+          '20g',
+          '20g',
+          '20g'
+        ],
+        description: 'Indicada para peixes de pequeno porte;'
+    },
+    {
+        id: 6,
+        name: 'Alimento para Peixe Alcon Gold Neon 30gr',
+        img: 'images/racao_passaro06.jpg',
+        price: [15.90, 15.90, 15.90],
+        sizes: [
+          '30g',
+          '30g',
+          '30g'
+        ],
+        description: 'Indicado para peixes de pequeno porte;'
+    }
+];
